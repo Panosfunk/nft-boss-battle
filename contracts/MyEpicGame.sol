f5f9a99de11649b4bf2f11b095b0c754b5514bf7
Panagiotis Giannoutsos <panosfunk@yahoo.gr>
2022-12-05 15:45:12 +0200

    chapter 4 section 'Checking for a character NFT' with errors.

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -10,10 +10,18 @@ import "hardhat/console.sol";
 import "./libraries/Base64.sol";
 
 
-contract MyEpicGame is ERC721{
+contract MyEpicGame is ERC721 {
   
   struct CharacterAttributes {
     uint characterIndex;
+    string name;
+    string imageURI;
+    uint hp;
+    uint maxHp;
+    uint ad;
+  }
+
+  struct BigBoss {
     string name;
     string imageURI;
     uint hp;
@@ -25,16 +33,26 @@ contract MyEpicGame is ERC721{
   Counters.Counter private _tokenIds;
 
   CharacterAttributes[] defaultCharacters;
+  BigBoss public bigBoss;
+
+  mapping(address => uint256) public nftHolders;
 
   mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
 
-  mapping(address => uint256) public nftHolders;
+  uint randNonce = 0;
+
+  event CharacterNFTMinted(address sender, uint256 tokenId, uint256 characterIndex);
+  event AttackComplete(address sender, uint256 newBossHp, uint256 newPlayerHp);
   
   constructor(
     string[] memory characterNames,
     string[] memory characterImageURIs,
     uint[] memory characterHp,
-    uint[] memory characterAd
+    uint[] memory characterAd,
+    string memory bossName,
+    string memory bossImageURI,
+    uint bossHP,
+    uint bossAd
   ) 
     ERC721("Heroes", "Hero") 
   {
@@ -54,6 +72,17 @@ contract MyEpicGame is ERC721{
     console.log("THIS IS MY GAME CONTRACT. NICE.");
 
     _tokenIds.increment();
+
+    bigBoss = BigBoss({
+      name: bossName,
+      imageURI: bossImageURI,
+      hp: bossHP,
+      maxHp: bossHP,
+      ad: bossAd
+    });
+
+    console.log("Done initializing boss %s w/ HP %s, img %s", bigBoss.name, bigBoss.hp, bigBoss.imageURI);
+
   }
 
   function mintCharacterNFT(uint _characterIndex) external {
@@ -74,33 +103,98 @@ contract MyEpicGame is ERC721{
     nftHolders[msg.sender] = newItemId;
 
     _tokenIds.increment();
+    emit CharacterNFTMinted(msg.sender, newItemId, _characterIndex);
   }
 
   function tokenURI(uint256 _tokenId) public view override returns (string memory) {
-  CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
+    CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
 
-  string memory strHp = Strings.toString(charAttributes.hp);
-  string memory strMaxHp = Strings.toString(charAttributes.maxHp);
-  string memory strAttackDamage = Strings.toString(charAttributes.ad);
+    string memory strHp = Strings.toString(charAttributes.hp);
+    string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+    string memory strAttackDamage = Strings.toString(charAttributes.ad);
 
-  string memory json = Base64.encode(
-    abi.encodePacked(
-      '{"name": "',
-      charAttributes.name,
-      ' -- NFT #: ',
-      Strings.toString(_tokenId),
-      '", "description": "This is an NFT that lets people play in the game Metaverse Slayer!", "image": "',
-      charAttributes.imageURI,
-      '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value":',strMaxHp,'}, { "trait_type": "Attack Damage", "value": ',
-      strAttackDamage,'} ]}'
-    )
-  );
+    string memory json = Base64.encode(
+      abi.encodePacked(
+        '{"name": "',
+        charAttributes.name,
+        ' -- NFT #: ',
+        Strings.toString(_tokenId),
+        '", "description": "This is an NFT that lets people play in the game Metaverse Slayer!", "image": "',
+        charAttributes.imageURI,
+        '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value":',strMaxHp,'}, { "trait_type": "Attack Damage", "value": ',
+        strAttackDamage,'} ]}'
+      )
+    );
 
-  string memory output = string(
-    abi.encodePacked("data:application/json;base64,", json)
-  );
-  
-  return output;
-}
+    string memory output = string(
+      abi.encodePacked("data:application/json;base64,", json)
+    );
+    
+    return output;
+  }
+
+  function attackBoss() public {
+    uint256 nftTokenIdOfPlayer = nftHolders[msg.sender];
+    CharacterAttributes storage player = nftHolderAttributes[nftTokenIdOfPlayer];
+    console.log("\nPlayer w/ character %s about to attack. Has %s HP and %s AD", player.name, player.hp, player.ad);
+    console.log("Boss %s has %s HP and %s AD", bigBoss.name, bigBoss.hp, bigBoss.ad);
+    require(player.hp > 0, "Hate to bring it to you but it looks like your character is dead homie.");
+
+    require(bigBoss.hp > 0, "STOP! ITS ALREADY DEAD YOU MONSTER.");
+
+    console.log("%s swings at %s...", player.name, bigBoss.name); 
+
+    if (bigBoss.hp < player.ad) {
+      bigBoss.hp = 0;
+      console.log("The boss is dead!");
+    } else {
+      if (randomInt(10) > 5) {                                 // by passing 10 as the mod, we elect to only grab the last digit (0-9) of the hash!
+          bigBoss.hp = bigBoss.hp - player.ad;
+          console.log("%s attacked boss. New boss hp: %s", player.name, bigBoss.hp);
+      } else {
+          console.log("%s missed!\n", player.name);
+      }
+    }
+
+    if (player.hp < bigBoss.ad) {
+      player.hp = 0;
+      console.log("Rip in peace player. You will not be remembered");
+    } else {
+      if (randomInt(10) > 5) {
+        player.hp = player.hp - bigBoss.ad;
+        console.log("%s attacked you! Your new hp is: %s", bigBoss.name, player.hp);
+      } else {
+        console.log("%s missed! Now is your chance to strike back!", bigBoss.name);
+      }
+    }
+    emit AttackComplete(msg.sender, bigBoss.hp, player.hp);
+  }
+
+  function randomInt(uint _modulus) internal returns(uint) {
+    randNonce++;
+    return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % _modulus;
+  }
+
+  function checkIfUserHasNFT() public view returns (CharacterAttributes memory) {
+    // Get the tokenId of the user's character NFT
+    uint256 userNftTokenId = nftHolders[msg.sender];
+    // If the user has a tokenId in the map, return their character.
+    if (userNftTokenId > 0) {
+      return nftHolderAttributes[userNftTokenId];
+    }
+    // Else, return an empty character.
+    else {
+      CharacterAttributes memory emptyStruct;
+      return emptyStruct;
+    }
+  }
+
+  function getAllDefaultCharacters() public view returns (CharacterAttributes[] memory) {
+    return defaultCharacters;
+  }
+
+  function getBigBoss() public view returns (BigBoss memory) {
+    return bigBoss;
+  }
 
 }
